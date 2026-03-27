b2e966eee5adabc7997cf1860e42127b743835a5
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-03-27 09:43:04 +0100

    feat: add grab sound to items

--- scripts/item/item.gd
+++ scripts/item/item.gd
@@ -2,10 +2,13 @@ extends CharacterBody3D
 
 class_name Item
 
+@export var grab_sound : AudioStreamPlayer3D
+
 var item_type : ItemType.type = ItemType.type.Item
 
 func _hold(new_parent: Node3D) -> Item:
 	print("Holding item" + str(item_type))
+	grab_sound.play()
 	var copy : Item = self.duplicate()
 	new_parent.add_child(copy)
 	copy.position = new_parent.position
